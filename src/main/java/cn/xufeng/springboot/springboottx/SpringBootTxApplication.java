988f786d4753e05c991a8971ec9857086051b1f6
CangXuan <2601708511@qq.com>
2019-05-05 16:34:24 +0800

    init repo

---
+++ src/main/java/cn/xufeng/springboot/springboottx/SpringBootTxApplication.java
@@ -0,0 +1,12 @@
+package cn.xufeng.springboot.springboottx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTxApplication.class, args);
+    }
+}
